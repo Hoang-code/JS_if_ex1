b8380efff8eb7ba74d984dd2bddf4222874b7404
Hoang-code <71956596+Hoang-code@users.noreply.github.com>
2020-10-19 19:29:11 +0700

    hoanthanh

---
+++ main.js
@@ -0,0 +1,9 @@
+function kiemtra(){
+
+    let nhaptraloi = document.getElementById("nhaptraloi").value
+    if (nhaptraloi == 'ECMAScript') {
+        alert('Right!');
+      } else {
+        alert("You don't know? ECMAScript!");
+      }
+}
